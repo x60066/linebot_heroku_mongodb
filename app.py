ef849513dcfd33e2a4c2c04e9baa380edfb3134b
vivobook JC <impelex.yen@gmail.com>
2022-05-12 01:21:31 +0800

    'ok'

--- app.py
+++ app.py
@@ -47,7 +47,7 @@ def handle_message(event):
     print(f.read())
     f.close()    
     
-    message = TextSendMessage(text=pretty_text)
+    message = TextSendMessage(text=f.read())
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
