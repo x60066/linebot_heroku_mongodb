c85d9c5fac2ac0a2b18a1eb983dd20c9fb0e2c35
vivobook JC <impelex.yen@gmail.com>
2022-05-12 00:02:14 +0800

    'ok'

--- app.py
+++ app.py
@@ -34,7 +34,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=event.message.text)
+    message = TextSendMessage(text='yen'+event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
