33b170879d3feacaf295364607c46d6e92c60d9f
vivobook JC <impelex.yen@gmail.com>
2022-05-11 23:58:19 +0800

    'ok'

--- app.py
+++ app.py
@@ -8,20 +8,9 @@ from linebot.exceptions import (
 )
 from linebot.models import *
 
-
-#======這裡是呼叫的檔案內容=====
-from message import *
-from new import *
-from Function import *
-from mongodb_function import *
-#======這裡是呼叫的檔案內容=====
-
-#======python的函數庫==========
-import  os
-#======python的函數庫==========
-
 app = Flask(__name__)
-static_tmp_path = os.path.join(os.path.dirname(__file__), 'static', 'tmp')
+
+# Channel Access Token
 # Channel Access Token
 line_bot_api = LineBotApi('TnS1t96rjGH7IvlqxyHqQ25e/e6jvVLOQfDUBaalj71kZu4miy9PRZk9r38DQJ2XcUV/mzlOc7b7hZZ62geq0gjDb37OsyGkpaG3+Gnkhu/Asvul9cj959G/hnoVAQ35H6V1XHdKmESJrF+yYF7RqAdB04t89/1O/w1cDnyilFU=')
 # Channel Secret1
@@ -34,7 +23,6 @@ def callback():
     signature = request.headers['X-Line-Signature']
     # get request body as text
     body = request.get_data(as_text=True)
-    write_one_data(eval(body))
     app.logger.info("Request body: " + body)
     # handle webhook body
     try:
@@ -43,84 +31,12 @@ def callback():
         abort(400)
     return 'OK'
 
-
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    msg = event.message.text
-    if '最新合作廠商' in msg:
-        message = imagemap_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '最新活動訊息' in msg:
-        message = buttons_message()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '註冊會員' in msg:
-        message = Confirm_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '旋轉木馬' in msg:
-        message = Carousel_Template()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '圖片畫廊' in msg:
-        message = test()
-        line_bot_api.reply_message(event.reply_token, message)
-    elif '功能列表' in msg:
-        message = function_list()
-        line_bot_api.reply_message(event.reply_token, message)
-
-    #======MongoDB操作範例======
-
-    elif '@讀取' in msg:
-        datas = read_many_datas()
-        datas_len = len(datas)
-        message = TextSendMessage(text=f'資料數量，一共{datas_len}條')
-        line_bot_api.reply_message(event.reply_token, message)
-
-    elif '@查詢' in msg:
-        datas = col_find('events')
-        message = TextSendMessage(text=str(datas))
-        line_bot_api.reply_message(event.reply_token, message)
-
-    elif '@對話紀錄' in msg:
-        datas = read_chat_records()
-        print(type(datas))
-        n = 0
-        text_list = []
-        for data in datas:
-            if '@' in data:
-                continue
-            else:
-                text_list.append(data)
-            n+=1
-        data_text = '\n'.join(text_list)
-        message = TextSendMessage(text=data_text[:5000])
-        line_bot_api.reply_message(event.reply_token, message)
-
-    elif '@刪除' in msg:
-        text = delete_all_data()
-        message = TextSendMessage(text=text)
-        line_bot_api.reply_message(event.reply_token, message)
-
-    #======MongoDB操作範例======
-
-    else:
-        message = TextSendMessage(text=msg)
-        line_bot_api.reply_message(event.reply_token, message)
-
-@handler.add(PostbackEvent)
-def handle_message(event):
-    print(event.postback.data)
-
-
-@handler.add(MemberJoinedEvent)
-def welcome(event):
-    uid = event.joined.members[0].user_id
-    gid = event.source.group_id
-    profile = line_bot_api.get_group_member_profile(gid, uid)
-    name = profile.display_name
-    message = TextSendMessage(text=f'{name}歡迎加入')
+    message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
-        
-        
+
 import os
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
