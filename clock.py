6324b0a2748ca34cec6f460b0297a6b064bcc83c
vivobook JC <impelex.yen@gmail.com>
2022-07-06 23:13:13 +0800

    'ok'

--- clock.py
+++ clock.py
@@ -7,10 +7,13 @@ sched = BlockingScheduler()
 @sched.scheduled_job('cron',  minute='*/2')
 def scheduled_job():
     print(f'{datetime.datetime.now().ctime()}')
+    print('Success 1')
     url = "https://chtran.herokuapp.com/"
     conn = urllib.request.urlopen(url)
         
     for key, value in conn.getheaders():
         print(key, value)
+        
+    print('Success 2')
 
 sched.start()
