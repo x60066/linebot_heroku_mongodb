2170f361a042e7df83053f95636ed080dfab7061
vivobook JC <impelex.yen@gmail.com>
2022-07-29 00:53:34 +0800

    'ok'

--- get_4G.py
+++ get_4G.py
@@ -63,7 +63,7 @@ class RAN:
         except:
             self.ran_search_index=-1
             
-        res = difflib.get_close_matches(key_search,trs_list_check,10,cutoff=0.7)
+        res = difflib.get_close_matches(key_search,trs_list_check,10,cutoff=0.8)
         print (res)
         
         for trs_search in res:
